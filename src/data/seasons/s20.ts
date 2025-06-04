f65aa2e553af892b949cc2b874141c84b6a16007
Mahir Shahriar Tamim <95174683+mahirshahriar1@users.noreply.github.com>
2025-06-04 15:53:11 +0600

    Update s20.ts

--- src/data/seasons/s20.ts
+++ src/data/seasons/s20.ts
@@ -23,7 +23,8 @@ export const participants: Participant = {
     Hadeon_99: "Fatin Anzum",
     tasin49: "Muhtadina Serniabat Tasin",
     pranto_11: "Farhin Ahmed Pranto",
-    technobyte: "Yousra Amin"
+    technobyte: "Yousra Amin",
+    Hrithik3: "Ifthikher Alam"
 };
 
 const contests: Record<number, Contest> = {
